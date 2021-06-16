657d4d22fe876df63af32ee073d0acd4a24b4f04
Marcelo Amorim <mamorim813@gmail.com>
2021-06-16 18:28:20 -0300

    Add Layout

---
+++ app/src/main/java/com/devventure/nicecalculator/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.devventure.nicecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
